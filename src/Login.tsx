d910c3633e3c0eabe6f6e24589eb838c4dfefe74
noahshotz <max.heinrich@uusimaa.de>
2023-09-13 01:24:49 +0200

    update to login screen

--- src/Login.tsx
+++ src/Login.tsx
@@ -10,6 +10,7 @@ function Login() {
         <div className="content">
           <h1>Login</h1>
           <h2>Das wird die Login seite</h2>
+          <p><a href="signup">Neues Konto erstellen</a></p>
         </div>
       </div>
     </React.Fragment>
