847c1694ecd6a10aa57c1757747f5205a2a249c4
noahshotz <max.heinrich@uusimaa.de>
2023-09-14 00:48:00 +0200

    updated sidebar html structure

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -9,20 +9,24 @@ const Sidebar: React.FC = () => {
     return (
         <React.Fragment>
             <div className="sidebar">
-                <div className="logo">
-                    <h1>My app</h1>
+                <div className="top-section">
+                    <div className="logo">
+                        <h1>My app</h1>
+                    </div>
+                    <div className="navigation">
+                        <ul>
+                            <li><a href="mymedia"><HiPhoto /> Photos</a></li>
+                            <li><a href="upload"><UploadIcon /> Upload</a></li>
+                            <li><a href="profile"><UserIcon /> Profile</a></li>
+                        </ul>
+                    </div>
                 </div>
-                <div className="navigation">
-                    <ul>
-                        <li><a href="mymedia"><HiPhoto /> Photos</a></li>
-                        <li><a href="upload"><UploadIcon /> Upload</a></li>
-                        <li><a href="profile"><UserIcon /> Profile</a></li>
-                    </ul>
+                <div className="bottom-section">
+                    <div className="logout">
+                        <Logout />
+                    </div>
+                    <img src="https://api.netlify.com/api/v1/badges/4340a9ce-6d53-4341-bbdc-43578b761064/deploy-status"></img>
                 </div>
-                <div className="logout">
-                    <Logout />
-                </div>
-                <img src="https://api.netlify.com/api/v1/badges/4340a9ce-6d53-4341-bbdc-43578b761064/deploy-status"></img>
             </div>
         </React.Fragment>
     )
