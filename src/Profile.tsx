876627cb363e41ae9a50c37f3215ef3505a9ae51
noahshotz <max.heinrich@uusimaa.de>
2023-09-13 02:06:36 +0200

    update to profile page

--- src/Profile.tsx
+++ src/Profile.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import Sidebar from './components/Sidebar';
+import { auth } from '../firebase'; // Import the auth object directly
 
 function Profile() {
+  const [userMailAddress, setUserMailAddress] = useState<string | null>('');
+
+  useEffect(() => {
+    // Check if the user is authenticated
+    if (auth.currentUser) {
+      // If the user is authenticated, set the user's email address
+      setUserMailAddress(auth.currentUser.email);
+    }
+  }, []);
 
   return (
     <React.Fragment>
@@ -9,11 +19,11 @@ function Profile() {
         <Sidebar />
         <div className="content">
           <h1>Profile</h1>
-          <h2>Ich geh jetzt pennen o_o</h2>
+          {userMailAddress && <h2>Hello {userMailAddress}</h2>}
         </div>
       </div>
     </React.Fragment>
-  )
+  );
 }
 
-export default Profile
+export default Profile;
